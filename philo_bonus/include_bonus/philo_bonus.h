f9cab862fc44f524c73b0976ae6eb425b9072eae
stankola <tommi.sankola@iki.fi>
2023-07-03 00:47:02 +0300

    Started work on bonus assignment

--- philo_bonus/include_bonus/philo_bonus.h
+++ philo_bonus/include_bonus/philo_bonus.h
@@ -1,19 +1,25 @@
 /******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo.h                                            :+:      :+:    :+:   */
+/*   philo_bonus.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tsankola <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/16 14:26:41 by tsankola          #+#    #+#             */
-/*   Updated: 2023/07/03 00:03:39 by tsankola         ###   ########.fr       */
+/*   Updated: 2023/07/03 00:20:53 by tsankola         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
 #ifndef PHILO_H
 # define PHILO_H
 # include <pthread.h>
-# define SLEEP_CYCLE 250	// waiting increment size in microseconds
+# include <sys/time.h>
+# include <semaphore.h>
+# define DEATH_SEMAPHORE_NAME "Morty"
+# define GRAB_SEMAPHORE_NAME "Grabby"
+# define FORK_SEMAPHORE_NAME "Forky"
+# define PRINT_SEMAPHORE_NAME "Phrinty"
+# define SLEEP_CYCLE 250
 
 /* Arguments:
 number_of_philosophers
@@ -32,12 +38,6 @@ enum e_args
 	max_meals = 4
 };
 
-enum e_mutex_indices
-{
-	DEATH_MUTEX_I = 0,
-	PRINT_MUTEX_I = 1
-};
-
 enum e_print_cases
 {
 	EAT,
@@ -46,13 +46,6 @@ enum e_print_cases
 	DIE,
 	THINK
 };
-
-typedef struct s_fork
-{
-	volatile int	taken;
-	pthread_mutex_t	grab_mutex;
-	pthread_mutex_t	fork_mutex;
-}	t_fork;
 
 typedef struct s_philosopher
 {
@@ -65,10 +58,7 @@ typedef struct s_philosopher
 	int				dead;
 	long int		prev_meal;
 	long int		inception;
-	t_fork			*r_utensil;
-	t_fork			*l_utensil;
 	volatile int	*death;
-	pthread_mutex_t	*mutexes;
 }	t_philosopher;
 
 long int	get_time_in_us();
@@ -82,11 +72,5 @@ void		eat(t_philosopher *phil);
 void		think(t_philosopher *phil);
 
 void		deep_think(t_philosopher *phil);
-
-int			take_fork(t_philosopher *phil, t_fork *f);
-
-void		drop_fork(t_fork *f);
-
-int			should_die(t_philosopher *phil);
 
 #endif
--- philo_bonus/src_bonus/philo_main_bonus.c
+++ philo_bonus/src_bonus/philo_main_bonus.c
@@ -1,21 +1,22 @@
 /******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo_main.c                                       :+:      :+:    :+:   */
+/*   philo_main_bonus.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tsankola <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/16 14:26:33 by tsankola          #+#    #+#             */
-/*   Updated: 2023/06/27 18:37:49 by tsankola         ###   ########.fr       */
+/*   Updated: 2023/06/27 21:10:02 by tsankola         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
+#include <fcntl.h>	// For O_CREAT
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
 #include <sys/time.h>
-#include "philo.h"
+#include "philo_bonus.h"
 
 // sleep
 // think
@@ -26,7 +27,6 @@ t_philosopher	*philosophize(t_philosopher *phil)
 	int			i;
 	static volatile int	death = 0;
 
-	phil->death = &death;
 	phil->prev_meal = get_time_in_us();
 	i = phil->id % 2;
 	while (! phil->dead)
@@ -34,85 +34,47 @@ t_philosopher	*philosophize(t_philosopher *phil)
 		actions[i](phil);
 		if ((i + 1) % 3 == 0 && ! phil->dead && phil->mm != 0 && ! --phil->mm)
 			return (phil);
+		should_die(phil);
 		i = (i + 1) % 3;
 	}
 	return (phil);
 }
 
-int	phallocate(t_fork **utensils, t_philosopher **phils, unsigned int args[],
-	pthread_mutex_t **mutexes)
+int	phallocate(t_philosopher **phils, unsigned int args[])
 {
 	*phils = malloc(sizeof(t_philosopher) * args[no_of_phils]);
 	if (*phils == NULL)
 		return (1);
-	*utensils = malloc(sizeof(t_fork) * args[no_of_phils]);
-	if (*utensils == NULL)
-	{
-		free(*phils);
-		return (1);
-	}
-	*mutexes = malloc(sizeof(pthread_mutex_t) * 2);
-	if (*mutexes == NULL)
-	{
-		free(*utensils);
-		free(*phils);
-		return (1);
-	}
-	pthread_mutex_init(*mutexes, NULL);	// could fail
-	pthread_mutex_init(&((*mutexes)[PRINT_MUTEX_I]), NULL);	// could fail
 	return (0);
-}
-
-int	finitialize(t_fork *f)
-{
-	f->taken = 0;
-	if (pthread_mutex_init(&f->fork_mutex, NULL) != 0
-		|| pthread_mutex_init(&f->grab_mutex, NULL) != 0)
-		return (0);
-	return (1);
 }
 
 t_philosopher	*phinitialize(unsigned int a[])
 {
-	t_fork			*fs;
 	t_philosopher	*ps;
-	pthread_mutex_t	*mutexes;
 	long int		now;
 	unsigned int	i;
 
-	if (phallocate(&fs, &ps, a, &mutexes))
+	if (phallocate(&ps, a))
 		return (NULL);
 	now = get_time_in_ms();
 	i = 0;
 	while (++i < a[no_of_phils])
 	{
-		finitialize(&fs[i]); // might fail
 		ps[i] = (t_philosopher){i + 1, a[time_to_die], a[time_to_eat],
-			a[time_to_sleep], a[max_meals], 0, 0, 0, now, &fs[i], &fs[i - 1], NULL, mutexes};
+			a[time_to_sleep], a[max_meals], 0, 0, now, NULL};
 	}
-	finitialize(&fs[0]); // might fail
 	ps[0] = (t_philosopher){1, a[time_to_die], a[time_to_eat], a[time_to_sleep],
-		a[max_meals], 0, 0, 0, now, &fs[0], &fs[a[no_of_phils] - 1], NULL, mutexes};
+		a[max_meals], 0, 0, now, NULL};
+	sem_open(FORK_SEMAPHORE_NAME, 0);
+	sem_open(PRINT_SEMAPHORE_NAME, 0);
 	return (ps);
 }
 
 void	phree(t_philosopher *phil, int philc)
 {
-	int	i;
-
-	i = 0;
-	while (i < philc)
-	{
-		pthread_mutex_destroy(&phil[i].r_utensil->fork_mutex);
-		pthread_mutex_destroy(&phil[i].r_utensil->grab_mutex);
-		i++;
-	}
-	pthread_mutex_destroy(phil->mutexes);
-	if (phil->r_utensil != NULL)
-		free(phil->r_utensil);
-	phil->l_utensil = NULL;
-	phil->r_utensil = NULL;
 	free(phil);
+	sem_close(sem_open(FORK_SEMAPHORE_NAME, 0));
+	sem_close(sem_open(PRINT_SEMAPHORE_NAME, 0));
 }
 
 pthread_t	*phacilitate(t_philosopher *phils, int philc)
@@ -143,7 +105,7 @@ int	main(int argc, char *argv[])
 	if (parse_args(argc, argv, args))
 		return (22);
 	if (argc == 5)
-		args[max_meals] = 0;	//TODO: Consider 0 as a command line argument. Shouldn't even start?
+		args[max_meals] = 0;
 	args[time_to_die] *= 1000;
 	args[time_to_eat] *= 1000;
 	args[time_to_sleep] *= 1000;
